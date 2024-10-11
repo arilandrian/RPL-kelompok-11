73c965d6f893519a0938a8413f33d430d2c34458
Ihram Syahputra <ihramsyih@gmail.com>
2024-10-11 21:22:36 +0800

    menambahkan file.sql

---
+++ Database/db_RumahMkn.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `tb_menu` (
+  `id_menu` int(11) NOT NULL,
+  `nama_menu` varchar(150) NOT NULL,
+  `harga` varchar(150) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `status_masakan` varchar(150) NOT NULL,
+  `gambar_masakan` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
