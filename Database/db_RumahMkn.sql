c418f604605ec3183c1b7d8bd3696b7b4fe7dca2
Ihram Syahputra <ihramsyih@gmail.com>
2024-10-11 22:19:21 +0800

    menambahkan tb_user

--- Database/db_RumahMkn.sql
+++ Database/db_RumahMkn.sql
@@ -7,4 +7,12 @@ CREATE TABLE `tb_menu` (
   `gambar_masakan` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `nama_user` varchar(150) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
  
