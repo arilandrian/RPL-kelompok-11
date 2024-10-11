1a6df0ba39cc64d000219d2db79412f34310a6ee
Ihram Syahputra <ihramsyih@gmail.com>
2024-10-11 22:51:17 +0800

    menghapus status makanan

--- Database/db_RumahMkn.sql
+++ Database/db_RumahMkn.sql
@@ -3,7 +3,6 @@ CREATE TABLE `tb_menu` (
   `nama_menu` varchar(150) NOT NULL,
   `harga` varchar(150) NOT NULL,
   `stok` int(11) NOT NULL,
-  `status_masakan` varchar(150) NOT NULL,
   `gambar_masakan` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
