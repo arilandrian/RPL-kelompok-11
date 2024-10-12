dba42b23289b74be5a2bb904bfaafd2c2d5edcbd
Ihram Syahputra <ihramsyih@gmail.com>
2024-10-12 23:07:15 +0800

    menambahkan koneksi.php

---
+++ connection/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	
+	$db = "db_RumahMKN";
+	$conn = mysqli_connect($host,$user,$pass,$db);
+	mysqli_select_db ($conn, $db);
+	
+	if (!$conn) {
+   		die('Maaf koneksi gagal: '. $connect->connect_error);
+	}
