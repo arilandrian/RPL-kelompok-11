651776436b38428d09715f5e7ed104dd95f160be
arilandrian <arilcoding2004@gmail.com>
2024-10-13 22:34:22 +0800

    update db dan upload tampilan terimakasih

--- db_warung.sql
+++ db_warung.sql
@@ -1,3 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 13, 2024 at 02:32 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,6 +26,7 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `tb_masakan`
 --
+
 CREATE TABLE `tb_masakan` (
   `id_masakan` int NOT NULL,
   `nama_masakan` varchar(100) NOT NULL,
@@ -27,16 +36,26 @@ CREATE TABLE `tb_masakan` (
   `stok` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `tb_masakan` (`id_masakan`, `nama_masakan`, `gambar_masakan`, `harga`, `status_masakan`, `stok`) VALUES
-(1, 'Nasi Goreng', 'nasi_goreng.jpg', '15000.00', 'tersedia', 7),
-(2, 'Mie Goreng', 'mie_goreng.jpg', '12000.00', 'tersedia', 8),
-(3, 'Ayam Penyet', 'ayam_penyet.jpg', '20000.00', 'tersedia', 10),
-(4, 'Sate Ayam', 'sate_ayam.jpg', '25000.00', 'tersedia', 10),
-(5, 'Es Teh', 'es_teh.jpg', '5000.00', 'tersedia', 8),
-(6, 'Es Jeruk', 'es_jeruk.jpg', '7000.00', 'tersedia', 8),
-(7, 'Kepala Ikan Bakar', 'kepala_ikan_bakar.jpg', '25000.00', 'tersedia', 10),
+--
+-- Dumping data for table `tb_masakan`
+--
 
- CREATE TABLE `tb_order` (
+INSERT INTO `tb_masakan` (`id_masakan`, `nama_masakan`, `gambar_masakan`, `harga`, `status_masakan`, `stok`) VALUES
+(1, 'Nasi Goreng', 'nasi_goreng.jpg', '15000.00', 'tersedia', 3),
+(2, 'Mie Goreng', 'mie_goreng.jpg', '12000.00', 'tersedia', 6),
+(3, 'Ayam Penyet', 'ayam_penyet.jpg', '20000.00', 'tersedia', 8),
+(4, 'Sate Ayam', 'sate_ayam.jpg', '25000.00', 'tersedia', 9),
+(5, 'Es Teh', 'es_teh.jpg', '5000.00', 'tersedia', 7),
+(6, 'Es Jeruk', 'es_jeruk.jpg', '7000.00', 'tersedia', 5),
+(7, 'Kepala Ikan Bakar', 'kepala_ikan_bakar.jpg', '25000.00', 'tersedia', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_order`
+--
+
+CREATE TABLE `tb_order` (
   `id_order` int NOT NULL,
   `id_user` int NOT NULL,
   `no_meja` varchar(20) NOT NULL,
@@ -45,12 +64,24 @@ INSERT INTO `tb_masakan` (`id_masakan`, `nama_masakan`, `gambar_masakan`, `harga
   `status` enum('belum diproses','sudah diproses') DEFAULT 'belum diproses'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesan`
+--
+
 CREATE TABLE `tb_pesan` (
   `id_pesan` int NOT NULL,
   `id_order` int NOT NULL,
   `id_masakan` int NOT NULL,
   `jumlah` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_riwayat_transaksi`
+--
 
 CREATE TABLE `tb_riwayat_transaksi` (
   `id_transaksi` int NOT NULL,
@@ -63,6 +94,12 @@ CREATE TABLE `tb_riwayat_transaksi` (
   `waktu_transaksi` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
 CREATE TABLE `tb_user` (
   `id_user` int NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -70,51 +107,106 @@ CREATE TABLE `tb_user` (
   `nama_user` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama_user`) VALUES
-(3, 'admin', 'admin123', 'Admin Warung'),
+--
+-- Dumping data for table `tb_user`
+--
 
+INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama_user`) VALUES
+(3, 'admin', 'admin123', 'Admin Warung');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_masakan`
+--
 ALTER TABLE `tb_masakan`
   ADD PRIMARY KEY (`id_masakan`);
 
+--
+-- Indexes for table `tb_order`
+--
 ALTER TABLE `tb_order`
   ADD PRIMARY KEY (`id_order`),
   ADD KEY `id_user` (`id_user`);
 
+--
+-- Indexes for table `tb_pesan`
+--
 ALTER TABLE `tb_pesan`
   ADD PRIMARY KEY (`id_pesan`),
   ADD KEY `id_order` (`id_order`),
   ADD KEY `id_masakan` (`id_masakan`);
 
+--
+-- Indexes for table `tb_riwayat_transaksi`
+--
 ALTER TABLE `tb_riwayat_transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
   ADD KEY `id_order` (`id_order`);
 
+--
+-- Indexes for table `tb_user`
+--
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_masakan`
+--
 ALTER TABLE `tb_masakan`
-  MODIFY `id_masakan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_masakan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
+--
+-- AUTO_INCREMENT for table `tb_order`
+--
 ALTER TABLE `tb_order`
-  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
+--
+-- AUTO_INCREMENT for table `tb_pesan`
+--
 ALTER TABLE `tb_pesan`
-  MODIFY `id_pesan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `id_pesan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 
+--
+-- AUTO_INCREMENT for table `tb_riwayat_transaksi`
+--
 ALTER TABLE `tb_riwayat_transaksi`
-  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_order`
+--
 ALTER TABLE `tb_order`
   ADD CONSTRAINT `tb_order_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
 
+--
+-- Constraints for table `tb_pesan`
+--
 ALTER TABLE `tb_pesan`
   ADD CONSTRAINT `tb_pesan_ibfk_1` FOREIGN KEY (`id_order`) REFERENCES `tb_order` (`id_order`),
   ADD CONSTRAINT `tb_pesan_ibfk_2` FOREIGN KEY (`id_masakan`) REFERENCES `tb_masakan` (`id_masakan`);
 
+--
+-- Constraints for table `tb_riwayat_transaksi`
+--
 ALTER TABLE `tb_riwayat_transaksi`
   ADD CONSTRAINT `tb_riwayat_transaksi_ibfk_1` FOREIGN KEY (`id_order`) REFERENCES `tb_order` (`id_order`);
 COMMIT;
---
+++ terima.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Terima Kasih</title>
+    <link rel="stylesheet" href="css/terima.css"> <!-- Tautkan ke file CSS -->
+</head>
+<body>
+    <h2>Terima Kasih Sudah Membeli!</h2>
+    <p>Pesanan Anda akan segera diproses.</p>
+    <a href="menu.php">Kembali ke Menu</a>
+</body>
+</html>
