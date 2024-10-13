21a852b83719119450fbf77cf7656c405565014b
arilandrian <arilcoding2004@gmail.com>
2024-10-13 23:07:34 +0800

    detail pesanan

---
+++ detail_pesanan.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Pesanan</title>
+    <link rel="stylesheet" href="css/admin.css"> <!-- Sesuaikan dengan file CSS yang ada -->
+</head>
+<body>
+    <div class="admin-container">
+        <h2>Detail Pesanan</h2>
+        <table>
+        <tr>
+            <th>No</th>
+            <th>Nama Lengkap Pembeli</th>
+            <th>No Meja</th>
+            <th>Total Harga</th>
+            <th>Waktu Pesan</th>
+            <th>Status</th>
+            <th>Makanan Dipesan</th>
+            <th>Aksi</th>
+        </tr>
+</body>
+</html>
