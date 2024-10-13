6c1c68f2bb57111b5e405553db325ec3048ce2a8
arilandrian <arilcoding2004@gmail.com>
2024-10-13 23:14:31 +0800

    pengaturan akun

---
+++ pengaturan_akun.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengaturan Akun</title>
+    <link rel="stylesheet" href="css/admin.css"> <!-- Sesuaikan dengan file CSS yang ada -->
+</head>
+<body>
+    <div class="admin-container">
+        <h2>Pengaturan Akun</h2>
+        <table>
+            <tr>
+                <th>No</th>
+                <th>Username</th>
+                <th>Nama Pengguna</th>
+                <th>Password</th>
+                <th>Aksi</th>
+            </tr>
+            </table>
+        <br>
+    </div>
+</body>
+</html>
